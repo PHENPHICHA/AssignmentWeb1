cf9b487c04dfb3259febf68953c4a9123148f5ec
PHENPHICHA <5910110236@psu.ac.th>
2019-04-23 00:20:46 +0700

    edit create api: get

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+const bt21 = require('./db')
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get('/bt21', (req, res) => {
+    res.json(bt21)
+  })
+
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
 })
-//test
