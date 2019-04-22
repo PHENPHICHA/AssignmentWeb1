856b591dad4c6a69733372b83a3e39eb404df8a0
PHENPHICHA <5910110236@psu.ac.th>
2019-04-23 01:19:12 +0700

    configuration firebase (get and post)

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
-const bt21 = require('./db')
 const bodyParser = require('body-parser')
+const firebase = require('firebase')
+
+const config = {
+    apiKey: "AIzaSyBmKla_IYy3zeZaH3Wnirpv2MsjNQ28KM4",
+    authDomain: "assignmentweb1-72964.firebaseapp.com",
+    databaseURL: "https://assignmentweb1-72964.firebaseio.com",
+    projectId: "assignmentweb1-72964",
+    storageBucket: "assignmentweb1-72964.appspot.com",
+    messagingSenderId: "865356969454"
+  };
+firebase.initializeApp(config);
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -11,17 +21,35 @@ app.get('/', (req, res) => {
 })
 
 app.get('/bt21', (req, res) => {
-    res.json(bt21)
+    var bt21Ref = firebase.database().ref('bt21');
+    bt21Ref.on('value', function(snapshot) {
+        res.send(snapshot.val());
+    });  
   })
 
 app.get('/bt21/:id', (req, res) => {
-    res.json(bt21.find(bt21 => bt21.id === req.params.id))
+    var id = req.params.id
+    firebase.database().ref('/bt21/' + id).once('value').then(function(snapshot) {
+      res.send(snapshot.val());
+    });
   })
   
 app.post('/bt21', (req, res) => {
-    bt21.push(req.body)
-    res.status(201).json(req.body)
-  })
+    var id = req.body.id
+    var name = req.body.name
+    var by = req.body.by
+    var price = req.body.price
+
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref('/bt21/' + id).set({
+        id: id,
+        name: name,
+        by: by,
+        price: price
+
+    });
+    res.send("Posted!");
+ })
 
 app.put('/bt21/:id', (req, res) => {
     const updateIndex = bt21.findIndex(bt21 => bt21.id === req.params.id)
